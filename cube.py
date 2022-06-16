68df4bb054b84ccd948d3afcd9b181581267cac3
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2022-06-16 10:52:57 +0300

    rubik's cube with playing keys

--- cube.py
+++ cube.py
@@ -1,118 +1,136 @@
 import pygame
+import random
 from pygame.locals import *
+
 from OpenGL.GL import *
 from OpenGL.GLU import *
-import numpy as np
 
 vertices = (
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1),
-    (0.333, -1, -1),
-    (0.333, 1, -1),
-    (0.333, 1, 1),
-    (0.333, -1, 1),
+    ( 1, -1, -1), ( 1,  1, -1), (-1,  1, -1), (-1, -1, -1),
+    ( 1, -1,  1), ( 1,  1,  1), (-1, -1,  1), (-1,  1,  1)
+)
+edges = ((0,1),(0,3),(0,4),(2,1),(2,3),(2,7),(6,3),(6,4),(6,7),(5,1),(5,4),(5,7))
+surfaces = ((0, 1, 2, 3), (3, 2, 7, 6), (6, 7, 5, 4), (4, 5, 1, 0), (1, 5, 7, 2), (4, 0, 3, 6))
+colors = ((1, 0, 0), (0, 1, 0), (1, 0.5, 0), (1, 1, 0), (1, 1, 1), (0, 0, 1))
 
-    (-0.333, -1, -1),
-    (-0.333, 1, -1),
-    (-0.333, 1, 1),
-    (-0.333, -1, 1),
+class Cube():
+    def __init__(self, id, N, scale):
+        self.N = N
+        self.scale = scale
+        self.init_i = [*id]
+        self.current_i = [*id]
+        self.rot = [[1 if i==j else 0 for i in range(3)] for j in range(3)]
 
-    (-1, -0.333, -1),
-    (1, -0.333, -1),
-    (1, -0.333, 1),
-    (-1, -0.333, 1),
+    def isAffected(self, axis, slice, dir):
+        return self.current_i[axis] == slice
 
-    (-1, 0.333, -1),
-    (1, 0.333, -1),
-    (1, 0.333, 1),
-    (-1, 0.333, 1),
+    def update(self, axis, slice, dir):
 
-    (-1, -1, -0.333),
-    (1, -1, -0.333),
-    (1, 1, -0.333),
-    (-1, 1, -0.333),
+        if not self.isAffected(axis, slice, dir):
+            return
 
-    (-1, -1, 0.333),
-    (1, -1, 0.333),
-    (1, 1, 0.333),
-    (-1, 1, 0.333),
-    )
+        i, j = (axis+1) % 3, (axis+2) % 3
+        for k in range(3):
+            self.rot[k][i], self.rot[k][j] = -self.rot[k][j]*dir, self.rot[k][i]*dir
 
-edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7),
-    (8,9),
-    (9,10),
-    (10,11),
-    (11,8),
-    (12,13),
-    (13,14),
-    (14,15),
-    (15,12),
+        self.current_i[i], self.current_i[j] = (
+            self.current_i[j] if dir < 0 else self.N - 1 - self.current_i[j],
+            self.current_i[i] if dir > 0 else self.N - 1 - self.current_i[i] )
 
-    (16, 17),
-    (17, 18),
-    (18, 19),
-    (19, 16),
+    def transformMat(self):
+        scaleA = [[s*self.scale for s in a] for a in self.rot]  
+        scaleT = [(p-(self.N-1)/2)*2.1*self.scale for p in self.current_i] 
+        return [*scaleA[0], 0, *scaleA[1], 0, *scaleA[2], 0, *scaleT, 1]
 
-    (20, 21),
-    (21, 22),
-    (22, 23),
-    (23, 20),
+    def draw(self, col, surf, vert, animate, angle, axis, slice, dir):
 
-    (24, 25),
-    (25, 26),
-    (26, 27),
-    (27, 24),
+        glPushMatrix()
+        if animate and self.isAffected(axis, slice, dir):
+            glRotatef( angle*dir, *[1 if i==axis else 0 for i in range(3)] )
+        glMultMatrixf( self.transformMat() )
 
-    (28, 29),
-    (29, 30),
-    (30, 31),
-    (31, 28),
-    )
+        glBegin(GL_QUADS)
+        for i in range(len(surf)):
+            glColor3fv(colors[i])
+            for j in surf[i]:
+                glVertex3fv(vertices[j])
+        glEnd()
 
-def Cube():
-    glBegin(GL_LINES)
-    for edge in edges:
-        for vertex in edge:
-            glVertex3fv(vertices[vertex])
+        glPopMatrix()
 
-    glEnd()
+class EntireCube():
+    def __init__(self, N, scale):
+        self.N = N
+        cr = range(self.N)
+        self.cubes = [Cube((x, y, z), self.N, scale) for x in cr for y in cr for z in cr]
+
+    def mainloop(self):
+
+        rot_cube_map  = { K_UP: (-1, 0), K_DOWN: (1, 0), K_LEFT: (0, -1), K_RIGHT: (0, 1)}
+        rot_slice_map = {
+            K_1: (0, 0, 1), K_2: (0, 1, 1), K_3: (0, 2, 1), K_4: (1, 0, 1), K_5: (1, 1, 1),
+            K_6: (1, 2, 1), K_7: (2, 0, 1), K_8: (2, 1, 1), K_9: (2, 2, 1),
+            K_F1: (0, 0, -1), K_F2: (0, 1, -1), K_F3: (0, 2, -1), K_F4: (1, 0, -1), K_F5: (1, 1, -1),
+            K_F6: (1, 2, -1), K_F7: (2, 0, -1), K_F8: (2, 1, -1), K_F9: (2, 2, -1),
+        }  
+
+        ang_x, ang_y, rot_cube = 0, 0, (0, 0)
+        animate, animate_ang, animate_speed = False, 0, 5
+        action = (0, 0, 0)
+        while True:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == KEYDOWN:
+                    if event.key in rot_cube_map:
+                        rot_cube = rot_cube_map[event.key]
+                    if not animate and event.key in rot_slice_map:
+                        animate, action = True, rot_slice_map[event.key]
+                if event.type == KEYUP:
+                    if event.key in rot_cube_map:
+                        rot_cube = (0, 0)
+
+            ang_x += rot_cube[0]*2
+            ang_y += rot_cube[1]*2
+
+            glMatrixMode(GL_MODELVIEW)
+            glLoadIdentity()
+            glTranslatef(0, 0, -40)
+            glRotatef(ang_y, 0, 1, 0)
+            glRotatef(ang_x, 1, 0, 0)
+
+            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+            if animate:
+                if animate_ang >= 90:
+                    for cube in self.cubes:
+                        cube.update(*action)
+                    animate, animate_ang = False, 0
+
+            for cube in self.cubes:
+                cube.draw(colors, surfaces, vertices, animate, animate_ang, *action)
+            if animate:
+                animate_ang += animate_speed
+
+            pygame.display.flip()
+            pygame.time.wait(10)
 
 def main():
+
     pygame.init()
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    glEnable(GL_DEPTH_TEST) 
 
-    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
-    glTranslatef(0.0, 0.0, -5)
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
-        glRotatef(1, 3, 1, 1)
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        Cube()
-        pygame.display.flip()
-        pygame.time.wait(10)
+    NewEntireCube = EntireCube(3, 1.5) 
+    NewEntireCube.mainloop()
 
-
-main()
+if __name__ == '__main__':
+    main()
+    pygame.quit()
+    quit()
